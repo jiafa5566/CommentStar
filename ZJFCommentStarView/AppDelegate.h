a073d436d1c69c975d37a61aeba4fb563393286a
jiafa <jiafa@dujiangdemacbook-pro.local>
2017-04-08 09:13:40 +0800

    Initial Commit

---
+++ ZJFCommentStarView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZJFCommentStarView
+//
+//  Created by 简而言之 on 2017/4/8.
+//  Copyright © 2017年 jiafa.apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZJFCommentStarView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZJFCommentStarView
+//
+//  Created by 简而言之 on 2017/4/8.
+//  Copyright © 2017年 jiafa.apple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
